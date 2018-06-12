279586e9aeaea6fdcc9d0ae8079c44acbb363a79
Erickowski <40204500+Erickowski@users.noreply.github.com>
2018-06-12 10:11:22 -0500

    Update Final Version

--- main.cpp
+++ main.cpp
@@ -1,9 +1,100 @@
 
-#include <iostream>
+#include "puzzle.h"
+#define MOVE_BABY 1
+#define MOVE_CRAB 2
+#define MOVE_CONE 3
+#define MOVE_MOM 4
 
-int main()
+
+int itemSideGlobal[4];
+
+int main(void)
 {
-  
+	int stateGame;
+	itemSideGlobal[BABY] = L;
+	itemSideGlobal[CRAB] = L;
+	itemSideGlobal[CONE] = L;
+	itemSideGlobal[MOM] = L;
+	
+	/*Wellcome and rules*/ 
+	welcomeRule();
+	do{
+		/*Display State*/
+		displayState(itemSideGlobal);
+		/*Capture move*/
+		switch(captureMove())
+		{
+			case MOVE_BABY:
+			{
+					if ((itemSideGlobal[BABY]==L)&&(itemSideGlobal[MOM]==L))
+					{
+						itemSideGlobal[BABY] = R;
+						itemSideGlobal[MOM] = R;
+					}
+					else if( (itemSideGlobal[BABY]==R)&&(itemSideGlobal[MOM]==R) )
+					{
+						itemSideGlobal[BABY] = L;
+						itemSideGlobal[MOM] = L;
+					}else
+					{
+						std::cout<<"movimiento invalido"<<std::endl;
+					}
+				break;}
+			case MOVE_CRAB:
+			{
+					if ((itemSideGlobal[CRAB]==L)&&(itemSideGlobal[MOM]==L))
+					{
+						itemSideGlobal[CRAB] = R;
+						itemSideGlobal[MOM] = R;
+					}
+					else if( (itemSideGlobal[CRAB]==R)&&(itemSideGlobal[MOM]==R) )
+					{
+						itemSideGlobal[CRAB] = L;
+						itemSideGlobal[MOM] = L;
+					}else
+					{
+						std::cout<<"movimiento invalido"<<std::endl;
+					}
+				break;}
+			case MOVE_CONE:
+			{
+					if ((itemSideGlobal[CONE]==L)&&(itemSideGlobal[MOM]==L))
+					{
+						itemSideGlobal[CONE] = R;
+						itemSideGlobal[MOM] = R;
+					}
+					else if( (itemSideGlobal[CONE]==R)&&(itemSideGlobal[MOM]==R) )
+					{
+						itemSideGlobal[CONE] = L;
+						itemSideGlobal[MOM] = L;
+					}else
+					{
+						std::cout<<"movimiento invalido"<<std::endl;
+					}
+				break;}
+			case MOVE_MOM:
+			{
+				if(itemSideGlobal[MOM]==L)
+				{
+					itemSideGlobal[MOM] = R;
+				}
+				else
+				{
+					itemSideGlobal[MOM] = L;
+				}
+				break;}
+			default:
+			{
+					std::cout<<"Error!"<<std::endl;
+					return -1;
+				break;}
+		}
+		//checkState
+		stateGame = checkState(itemSideGlobal);
+		//displayMessage
+		displayMessage(stateGame);
+	}while(stateGame==AGA);
+	displayState(itemSideGlobal);
   return 0;
 }
 
--- puzzle.h
+++ puzzle.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define WIN 1 //Ganador
 #define LOS 2 //Perdedor
@@ -11,20 +13,42 @@
 #define BABY 0
 #define CRAB 1
 #define CONE 2
+#define MOM 3
+
+void welcomeRule(void)
+{
+	std::cout<<"Welcome to the Puzzle of the Baby, the Crab, and the Cone.\n";
+	std::cout<<"One hot summer day, Mama crossed the river to the left\n";
+	std::cout<<"bank take a break from programming and buy an ice cream.\n";
+	std::cout<<"She brought her baby and her pet crab. On the return\n";
+	std::cout<<"she came to the river and was faced with a dilemma: how\n";
+	std::cout<<"could they all cross without disaster? The ferry holds\n\n";
+	std::cout<<"Mama and just one other item. The problem is that\n";
+	std::cout<<"* if the baby is left with the cone, it spoils its dinner.\n";
+	std::cout<<"* if the baby is left with the crab, the crab bites the baby.\n";
+	std::cout<<"Please help Mama make her journey across the river!\n\n";  
+	std::cout<<"Presione 'Enter' para continuar";
+	getchar();
+	//system("clear");
+	}
 
 int checkState(int itemSide[3])//Posicion 0 = Baby,Posicion 1 = Crab,Posicion 2 = Cone,
 {
 	/* RULES 
-	 Baby Crab Cone | flag
-	 * R    R    R    WIN
-	 * R    R    L    LOS
-	 * R    L    R    LOS
- 	 * L    R    R    AGA
-	 * L    L    L    AGA
-	 * L    L    R    LOS
-	 * L    R    L    LOS
-	 * R    L    L    AGA
-	 */
+	 Baby Crab Cone MOM| flag
+	 * R    R    R   X   WIN 
+	 * R    R    L   R   AGA 
+	 * R    R    L   L   LOS 
+	 * R    L    R   R   AGA
+	 * R    L    R   L   LOS
+ 	 * L    R    R   X   AGA
+	 * L    L    L   X   AGA
+	 * L    L    R   L   AGA
+	 * L	L 	 R 	 R   LOS 
+	 * L    R    L   R   LOS 
+	 * L    R    L   L   AGA 
+	 * R    L    L   X   AGA
+	 */ 
 	int flag = ERR;
 	
 	/*WIN*/
@@ -35,12 +59,19 @@ int checkState(int itemSide[3])//Posicion 0 = Baby,Posicion 1 = Crab,Posicion 2 
 	/*LOS*/
 	if( (itemSide[BABY]==R)&&(itemSide[CRAB]==R)&&(itemSide[CONE]==L) )
 	{
-		flag =  LOS; 
+		if (itemSide[MOM] == R)
+		flag =  AGA;
+		else 
+		flag = LOS; 
 	}
 	/*LOS*/
 	if( (itemSide[BABY]==R)&&(itemSide[CRAB]==L)&&(itemSide[CONE]==R) )
 	{
-		flag =  LOS; 
+		if (itemSide[MOM] == R)
+		flag =  AGA;
+		else 
+		flag = LOS;
+		 
 	}
 	/*AGA*/
 	if( (itemSide[BABY]==L)&&(itemSide[CRAB]==R)&&(itemSide[CONE]==R) )
@@ -50,42 +81,79 @@ int checkState(int itemSide[3])//Posicion 0 = Baby,Posicion 1 = Crab,Posicion 2 
 	/*AGA*/
 	if( (itemSide[BABY]==L)&&(itemSide[CRAB]==L)&&(itemSide[CONE]==L) )
 	{
-		flag =  AGA; 
+		if (itemSide[MOM] == L)
+		flag =  AGA;
+		else 
+		flag = LOS;
 	}
 	/*LOS*/
 	if( (itemSide[BABY]==L)&&(itemSide[CRAB]==L)&&(itemSide[CONE]==R) )
 	{
-		flag =  LOS; 
+		if (itemSide[MOM] == L)
+		flag =  AGA;
+		else 
+		flag = LOS;
 	}
 	/*LOS*/
 	if( (itemSide[BABY]==L)&&(itemSide[CRAB]==R)&&(itemSide[CONE]==L) )
 	{
-		flag =  LOS; 
+		if (itemSide[MOM] == L)
+		flag =  AGA;
+		else 
+		flag = LOS;
 	}
 	/*AGA*/
-	if( (itemSide[BABY]==R)&&(itemSide[CRAP]==L)&&(itemSide[CONE]==L) )
+	if( (itemSide[BABY]==R)&&(itemSide[CRAB]==L)&&(itemSide[CONE]==L) )
 	{
-		flag =  LOS; 
+		flag =  AGA; 
 	}
 	return flag;
 }
 /**
  * @autor Erick
  * Funcion captura movimiento para actualizar los estados
- * @param arreglo actual de los personajes
- * @return arreglo con las posiciones capturadas
+ * @param 
+ * @return Personaje a mover
  * */
-int* captureMove(int itemSide[3])
-{
-	int estadoBaby;
-	int estadoCrab;
-	int estadoCone;
-	std::cout<<"Ingrese pocision de Baby: ";
-	std::cin>>estadoBaby;
-	std::cout<<"Ingrese pocision de Crab: ";
-	std::cin>>estadoCrab;
-	std::cout<<"Ingrese pocision de Cone: ";
-	std::cin>>estadoCone;
-	return itemSide;
-	
+int captureMove()
+{	
+	int move;
+	std::cout<<"Realize un movimiento:"<<std::endl;
+	std::cout<<"1 Mover Baby:"<<std::endl;
+	std::cout<<"2 Mover Crab:"<<std::endl;
+	std::cout<<"3 Mover Cone:"<<std::endl;
+	std::cout<<"4 Mover MOM:"<<std::endl;
+	std::cin>>move;
+	return move;
 }
+void displayState(int itemSide[3])
+{
+	std::cout<<"The Baby is on the "<<(itemSide[0]==-1 ? "left":"rigth")<<std::endl;
+	std::cout<<"The Crab is on the "<<(itemSide[1]==-1 ? "left":"rigth")<<std::endl;
+	std::cout<<"The Cone is on the "<<(itemSide[2]==-1 ? "left":"rigth")<<std::endl;
+	std::cout<<"The Mom is on the  "<<(itemSide[3]==-1 ? "left":"rigth")<<std::endl;
+}
+int displayMessage(int stateGame)
+{
+	switch(stateGame)
+	{
+		case WIN:
+		{
+			std::cout<<"Your winner! :)"<<std::endl;
+			break;}
+		case LOS:
+		{
+			std::cout<<"game over! :("<<std::endl;
+			break;}
+		case AGA:
+		{
+			std::cout<<"Next move! :)"<<std::endl;
+			break;}
+		case ERR:
+		{
+			std::cout<<"Error! :)"<<std::endl;
+			return -1;
+			break;}
+	}
+}
+
